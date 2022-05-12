bc2c0af4d2265dacf44ffce9ba6a58f169eac652
Fred Lahode <lahode@infomaniak.ch>
2022-05-12 11:45:21 +0200

    Disallow OPTION request because it causes CORS issues

--- www/index.php
+++ www/index.php
@@ -12,6 +12,9 @@
   header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
   header("Access-Control-Max-Age: 3600");
   header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+    exit; // OPTIONS request wants only the policy, we can stop here
+  }
 
   /**
    * Récupération d'une partie de l'url en entrée 
